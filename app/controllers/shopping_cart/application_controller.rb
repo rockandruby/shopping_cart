ad0a3c791cf1338cf3856478b05668ea970edcc9
Igor <igor.pogosian@small-team.com>
2016-08-13 16:22:01 +0300

    bugs

--- app/controllers/shopping_cart/application_controller.rb
+++ app/controllers/shopping_cart/application_controller.rb
@@ -1,7 +1,7 @@
 module ShoppingCart
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :authenticate_user!
+    before_action "authenticate_#{ShoppingCart.user_class.downcase}!".to_sym
     alias_method :current_user, "current_#{ShoppingCart.user_class.downcase}".to_sym
 
     rescue_from CanCan::AccessDenied do |exception|
