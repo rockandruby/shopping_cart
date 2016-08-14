ccb70399e6d37b33c74e04b68fd273cdc4097260
Igor <igor.pogosian@small-team.com>
2016-08-14 10:49:44 +0300

    addded dependencies

--- lib/shopping_cart/engine.rb
+++ lib/shopping_cart/engine.rb
@@ -1,3 +1,9 @@
+Gem.loaded_specs['shopping_cart'].dependencies.each do |d|
+  require d.name
+end
+
+require 'shopping_cart/engine'
+
 module ShoppingCart
   class Engine < ::Rails::Engine
     isolate_namespace ShoppingCart
