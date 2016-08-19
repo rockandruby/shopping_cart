f594950d14eb1c3e9a9e37c283605cc9f5b8ed92
Igor <igor.pogosian@small-team.com>
2016-08-19 17:42:58 +0300

    view generator in process

--- lib/generators/shopping_cart/views_generator.rb
+++ lib/generators/shopping_cart/views_generator.rb
@@ -1,5 +1,11 @@
 module ShoppingCart
   class ViewsGenerator < Rails::Generators::Base
+    source_root File.expand_path("../../../../app/views", __FILE__)
 
+    desc 'copy engine views to app'
+
+    def copy_views
+      directory 'shopping_cart', 'app/views/shopping_cart'
+    end
   end
 end
